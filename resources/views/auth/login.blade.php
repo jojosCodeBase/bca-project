768b61b983ffe78adbe7f29b4d181af522da872d
Bikram Das <bikramdas736136@gmail.com>
2024-03-15 13:31:02 +0530

    login form style change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,70 +5,138 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result Analysis System | Login</title>
-    <link rel="icon" href="{{asset('assets/images/smitlogo2.ico')}}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/smitlogo2.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
-<body>
+<body style="background-color: aliceblue">
+    {{-- <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-5 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
+
+                        <div class="text-center mt-4">
+                            <h1 class="h2">Welcome to SMIT-NSS Portal</h1>
+                            <p class="lead">
+                                Sign in to your account to continue
+                            </p>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-4">
+                                    <div class="text-center">
+                                        <img src="https://smitnss.darjeelingcab.in/assets/img/icons/admin-icon.png" alt="admin_image"
+                                            class="img-fluid rounded-circle" width="132" height="132" />
+                                    </div>
+                                    <form action="https://smitnss.darjeelingcab.in/login" method="POST">
+                                        <input type="hidden" name="_token" value="OTgqE6cLAnb1sEhQsnXVr4p3gSJEvPKqpR43C9dJ" autocomplete="off">                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input class="form-control form-control-lg" type="email" name="email"
+                                                id="email" placeholder="Enter your email" required>
+                                            <span class="text-danger" id="email-error"></span>
+
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input class="form-control form-control-lg" type="password" name="password"
+                                                id="password" placeholder="Enter your password" required>
+                                            <span class="text-danger" id="password-error"></span>
+                                        </div>
+                                        <div>
+                                            <label class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="remember-me"
+                                                    name="remember-me" checked>
+                                                <span class="form-check-label">
+                                                    Remember me
+                                                </span>
+                                            </label>
+                                        </div>
+                                        <div class="text-center mt-3">
+                                            <input type="submit" class="btn btn-lg btn-primary w-100 fw-bold fs-4"
+                                                value="Login">
+                                        </div>
+                                        <div class="mt-2 text-center">
+                                            <a href="https://smitnss.darjeelingcab.in/forgot-password">Forgot password?</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main> --}}
     <main class="d-flex w-100">
         <div class="container">
-            <div class="row d-flex justify-content-center align-content-center">
-                <div class="text-center my-5">
+            <div class="row d-flex justify-content-center">
+                <div class="text-center mt-5 mb-3">
                     <h1 class="h2">Result Analysis Portal</h1>
                 </div>
 
                 <div class="col-7">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row py-4">
-                                <div class="col-5">
-                                    <div class="m-sm-4 text-center mt-2 mb-2">
-                                        <img src="{{ asset('assets/images/smitlogo2.jpg') }}" alt=""
-                                            class="img-fluid rounded-circle" width="200" height="200">
+                    <div class="login">
+                        <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                            <div class="card-body">
+                                <div class="row py-4">
+                                    <div class="col-5">
+                                        <div class="m-sm-4 text-center mt-2 mb-2">
+                                            <img src="{{ asset('assets/images/smitlogo2.jpg') }}" alt=""
+                                                class="img-fluid" width="200" height="200">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-7">
-                                    <p class="fs-5 fw-bold text-center">
-                                        Log In
-                                    </p>
+                                    <div class="col-7">
+                                        <p class="fs-5 fw-bold text-center">
+                                            Log In
+                                        </p>
 
-                                    <form method="POST" action="{{ route('login') }}" class="mx-4" id="enterFunc">
-                                        @csrf
-                                        <div class="mb-4">
-                                            <div class="input-group">
-                                                <input type="text" class="form-control" placeholder="Username" name="email"
-                                                    aria-label="Username" aria-describedby="basic-addon1" required>
-                                                <span class="input-group-text"><i class="bi bi-person-fill"></i></span>
+                                        <form method="POST" action="{{ route('login') }}" class="mx-4"
+                                            id="enterFunc">
+                                            @csrf
+
+                                            <div class="mb-4">
+                                                <div class="input-group">
+                                                    <input type="text" class="form-control" placeholder="Username"
+                                                        name="email" aria-label="Username"
+                                                        aria-describedby="basic-addon1" required>
+                                                    <span class="input-group-text  bg-transparent"><i
+                                                            class="bi bi-person-fill"></i></span>
+                                                </div>
+                                                @error('email')
+                                                    <span class="text-danger fs-6">{{ $message }}</span>
+                                                @enderror
                                             </div>
-                                            @error('email')
-                                                <span class="text-danger fs-6">{{ $message }}</span>
-                                            @enderror
-                                        </div>
-                                        <div class="mb-4">
-                                            <div class="input-group">
-                                                <input type="password" class="form-control" name="password" id="password"
-                                                    placeholder="Password" required>
-                                                <span class="input-group-text"><i class="bi bi-lock-fill"></i></span>
+                                            <div class="mb-4">
+                                                <div class="input-group">
+                                                    <input type="password" class="form-control" name="password"
+                                                        id="password" placeholder="Password" required>
+                                                    <span class="input-group-text bg-transparent"><i
+                                                            class="bi bi-lock-fill"></i></span>
+                                                </div>
+                                                @error('password')
+                                                    <span class="text-danger fs-6">{{ $message }}</span>
+                                                @enderror
                                             </div>
-                                            @error('password')
-                                                <span class="text-danger fs-6">{{ $message }}</span>
-                                            @enderror
-                                        </div>
-                                        <div class="mb-4">
-                                            <label for="checkbox" class="form-check user-select-none">Show Password
-                                                <input type="checkbox" class="form-check-input" value="remember_me"
-                                                    name="remember-me" id="checkbox">
-                                                <span class="form-check-label" id="#"></span>
-                                            </label>
-                                        </div>
-                                        <div class="form-group mb-3">
-                                            <input type="submit" class="btn btn-primary w-100 fw-bold" value="Login" id="">
-                                        </div>
-                                        <div class="form-group text-center forgot-password">
-                                            <a href="{{ route('password.request') }}">Forgot password?</a>
-                                        </div>
+                                            <div class="mb-4">
+                                                <label for="checkbox" class="form-check user-select-none">Show Password
+                                                    <input type="checkbox" class="form-check-input" value="remember_me"
+                                                        name="remember-me" id="checkbox">
+                                                    <span class="form-check-label" id="#"></span>
+                                                </label>
+                                            </div>
+                                            <div class="form-group mb-3">
+                                                <input type="submit" class="btn btn-primary w-100 fw-bold"
+                                                    value="Login" id="">
+                                            </div>
+                                            <div class="form-group text-center forgot-password">
+                                                <a href="{{ route('password.request') }}">Forgot password?</a>
+                                            </div>
+                                    </div>
+
                                     </form>
                                 </div>
                             </div>
@@ -81,4 +149,5 @@
 </body>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/js/showPassword.js') }}"></script>
+
 </html>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,19 +1,18 @@
-
 @extends('layouts.main')
 @section('title', 'Profile Edit')
 @section('content')
     <!-- Begin Page Content -->
     <div class="container-fluid">
         @if (session('status'))
-        <div class="alert alert-success">
-            <span>{{ session('status') }}</span>
-        </div>
+            <div class="alert alert-success">
+                <span>{{ session('status') }}</span>
+            </div>
         @endif
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Profile</h1>
         </div>
-        <div class="row">
-            <div class="col">
+        <div class="grid">
+            <div class="g-col-6">
                 <div class="card">
                     <div class="card-body">
                         <h4>Profile Information</h4>
@@ -22,24 +21,26 @@
                             @csrf
                             @method('patch')
 
-                            <div class="form-group">
+                            <div class="form-group mt-2">
+                                <label class="form-label">Faculty Id.</label>
+                                <input type="text" class="form-control" name="text" value="202116057" readonly>
+                            </div>
+                            <div class="form-group mt-2">
                                 <label class="form-label">Name</label>
                                 <input type="text" class="form-control" name="name" value="{{ $user->name }}">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-2">
                                 <label class="form-label">Email</label>
                                 <input type="email" class="form-control" name="email" value="{{ $user->email }}">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <input type="submit" class="btn btn-primary w-25" value="Save">
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col">
+            <div class="g-col-6">
                 <div class="card">
                     <div class="card-body">
                         <h4>Update Password</h4>
@@ -51,16 +52,16 @@
                                 <label class="form-label">Current Password</label>
                                 <input type="password" class="form-control" name="current_password">
                                 <div class="mt-2 text-danger">
-                                    @if($errors->updatePassword->has('current_password'))
+                                    @if ($errors->updatePassword->has('current_password'))
                                         <span>{{ $errors->updatePassword->first('current_password') }}</span>
                                     @endif
                                 </div>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group ">
                                 <label class="form-label">New Password</label>
                                 <input type="password" class="form-control" name="password">
                                 <div class="mt-2 text-danger">
-                                    @if($errors->updatePassword->has('password'))
+                                    @if ($errors->updatePassword->has('password'))
                                         <span>{{ $errors->updatePassword->first('password') }}</span>
                                     @endif
                                 </div>
@@ -69,13 +70,16 @@
                                 <label class="form-label">Confirm Password</label>
                                 <input type="password" class="form-control" name="password_confirmation">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <input type="submit" class="btn btn-primary w-25" value="Save">
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="row mt-4">
+
         </div>
     </div>
 @endsection
