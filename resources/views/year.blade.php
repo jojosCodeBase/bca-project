606980ef50ce8c1b141d78e4374e994c2d1aac17
Bikram Das <bikramdas736136@gmail.com>
2024-04-25 23:47:25 +0530

    minor changes

--- resources/views/year.blade.php
+++ resources/views/year.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Year Wise')
 @section('breadcrumb', 'Year Wise')
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="card">
             <div class="card-body">
                 <div class="row">
