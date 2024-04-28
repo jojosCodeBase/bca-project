55c2233cf8cb0e52ac84037dec5f4ab41816288b
Bikram Das <bikramdas736136@gmail.com>
2024-04-28 12:07:02 +0530

    Add charts value

--- resources/views/mca.blade.php
+++ resources/views/mca.blade.php
@@ -7,7 +7,7 @@
             <div class="card-body">
                 <div class="row">
                     <h4 class="py-2 text-custom">MCA Report</h4>
-                    <div class="col-xl-2 col-lg-3 col-md-3 col-6 mt-1">
+                    {{-- <div class="col-xl-2 col-lg-3 col-md-3 col-6 mt-1">
                         <select name="Year" id="years" class="form-select">
                             <option value="#">Select year</option>
                             <option value="2019">2019</option>
@@ -17,31 +17,40 @@
                             <option value="2023">2023</option>
                             <option value="2024">2024</option>
                         </select>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-6 mt-1">
+                    </div> --}}
+                    {{-- <div class="col-xl-3 col-lg-3 col-md-3 col-6 mt-1">
                         <select name="branch" id="brance" class="form-select">
                             <option value="branch">Select a course</option>
                             <option value="BCA">BCA</option>
                             <option value="MCA">MCA</option>
                         </select>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-6 mt-1">
+                    </div> --}}
+                    {{-- <div class="col-xl-3 col-lg-3 col-md-3 col-6 mt-1">
                         <select name="branch" id="brance" class="form-select">
                             <option value="branch">Select a semester</option>
                             <option value="odd">Odd</option>
                             <option value="even">Even</option>
                         </select>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-3 col-6 mt-1">
+                    </div> --}}
+                    <div class="col-xl-5 col-lg-3 col-md-3 col-6 mt-1">
                         <select name="Course" id="courseid" class="form-select">
-                            <option value="#">Select a Course Id</option>
+                            <option value="#">Select Subject Id</option>
                             <option value="CA1601">CA1601</option>
                             <option value="CA1603">CA1603</option>
                             <option value="CA1636">CA1636</option>
                             <option value="CA1637">CA1637</option>
                         </select>
                     </div>
-                    <div class="col-xl-1 d-flex">
+                    <div class="col-xl-5 col-lg-3 col-md-3 col-6 mt-1">
+                        <select name="Course" id="courseid" class="form-select">
+                            <option value="#">Select Subject Name</option>
+                            <option value="">Python</option>
+                            <option value="">Software Engineering</option>
+                            <option value="">Data Analytics</option>
+                            <option value="">Security</option>
+                        </select>
+                    </div>
+                    <div class="col-xl-2 d-flex">
                         <input type="submit" name="#" id="#" value="Fetch" class="btn btn-primary ">
                     </div>
                 </div>
@@ -74,14 +83,14 @@
         }
 
         // Generate labels for 11 years
-        const labels = generateYears(25);
+        const labels = generateYears(10);
 
         // Data configuration
         const data = {
             labels: labels,
             datasets: [{
-                label: 'Yearly Direct Attainment',
-                data: [ /* Add your data for 11 years here */ ],
+                label: 'Subject Report',
+                data: [1.5,2.1,1.8,2.1,1.9,2.6,1.5,1.8,2.5,1.0/* Add your data for 11 years here */ ],
                 fill: false,
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1
