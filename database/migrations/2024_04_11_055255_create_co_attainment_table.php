55b8400cbb5af70ce6bc10b1e031a6c65cf82cdc
Kunsang Moktan <kunsangmoktan11@gmail.com>
2024-05-04 12:32:29 +0530

    upload error fixed for co_attainment

--- database/migrations/2024_04_11_055255_create_co_attainment_table.php
+++ database/migrations/2024_04_11_055255_create_co_attainment_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->json('s2');
             $table->json('assignment');
             $table->json('end_sem');
-            $table->json('total');
             $table->timestamps();
         });
     }
